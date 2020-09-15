c348a1b19823b5f4bcde64c5a62292f1b0c1ae2b
artofpaganini@gmail.com <Grozbeckert2015>
2020-09-15 10:38:29 +0300

    добавил поддержку  экранов, добавил сохранение данных, протестировал на разных  размерах  экранов

--- app/src/main/java/by/andersen/intern/landscapeorientation/MainActivity.java
+++ app/src/main/java/by/andersen/intern/landscapeorientation/MainActivity.java
@@ -1,11 +1,13 @@
 package by.andersen.intern.landscapeorientation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,14 +15,16 @@ public class MainActivity extends AppCompatActivity {
 
 
     private Button button;
-    private TextView textView;
+    private EditText editText;
+    private String sometext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.text_view);
+        editText = findViewById(R.id.edit_text);
         button = findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -35,5 +39,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        if (savedInstanceState != null) {
+           sometext =  savedInstanceState.getString("text");
+           editText.setText(sometext);
+        }
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("text", editText.getText().toString());
+    }
+
+
 }
